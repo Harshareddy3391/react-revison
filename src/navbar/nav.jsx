416ee8f5a740e07e747273d2d5efc02e576fa319
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-05-07 11:44:08 +0530

    change

--- src/navbar/nav.jsx
+++ src/navbar/nav.jsx
@@ -4,12 +4,15 @@ const Navbar = () => {
   return (
     <div>
         <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
-            
-            <ul className='btn btn-primary'>
+            <div className='ms auto'>
+                <ul className='btn btn-primary'>
                 <li>home</li>
                 <li>about</li>
                 <li>contact</li>
                 </ul>
+            </div>
+            
+             
                 </nav>      
     </div>
   )
