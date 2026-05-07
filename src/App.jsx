66a1d76e6a64164bc33391262083f5243c1e718a
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-05-07 11:30:40 +0530

    updata

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Home from './home'
+import Navbar from './navbar/nav'
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Home/>
       
       
