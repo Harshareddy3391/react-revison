e42be8c20a6288fc97caf9948e872d4559e6d51c
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-05-07 11:24:04 +0530

    first

---
+++ src/navbar/contact.jsx
@@ -0,0 +1,21 @@
+/*
+usage : 
+*/
+
+
+import React from 'react'
+import { useEffect } from 'react'
+
+const Contact = () => {
+
+    useEffect()
+  return (
+    <div>
+
+
+      
+    </div>
+  )
+}
+
+export default Contact
