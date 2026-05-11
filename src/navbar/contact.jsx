3fa7e4a410d52a39af95051c9573e98de242c5f6
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-05-11 10:51:53 +0530

    api

--- src/navbar/contact.jsx
+++ src/navbar/contact.jsx
@@ -3,15 +3,28 @@ usage :
 */
 
 
+import { Axios } from 'axios'
 import React from 'react'
-//import { useEffect } from 'react'
+import { useEffect,useState } from 'react'
 
 const Contact = () => {
+  let [data,setData] = useState({})
+
+  useEffect(()=>{
+    Axios.get("")
+    .then((res)=>{
+      setData(res.data)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })    
+  },[])
 
    
   return (
     <div>
       <h1>Contact page</h1>
+      <pre>{JSON.stringify(data)}</pre>
 
 
       
