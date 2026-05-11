7738f5f57d4418ad68aaa4f1011ff15d994253c1
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-05-11 11:12:03 +0530

    catch

--- src/navbar/contact.jsx
+++ src/navbar/contact.jsx
@@ -12,7 +12,8 @@ const Contact = () => {
   let [data,setData] = useState({})
 
   useEffect(()=>{
-    Axios.get("")
+    let url="https://gist.githubusercontent.com/narasimhareddyprostack/7e344f346f47bc53a889d78b5258d0c9/raw/56d531cb936d9c79e2417e5d0e5d8c9c876800f2/contactlist"
+    Axios.get(url)
     .then((res)=>{
       setData(res.data)
     })
