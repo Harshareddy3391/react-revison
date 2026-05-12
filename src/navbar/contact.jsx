7863540feb48476cc67b95579df99b0eb5e63f0a
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-05-12 11:55:00 +0530

    completed

--- src/navbar/contact.jsx
+++ src/navbar/contact.jsx
@@ -1,93 +1,81 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Contact = () => {
 
   let [data, setData] = useState([])
+  let [selectedContact, setSelectedContact] = useState(null)
 
   useEffect(() => {
-
-    axios.get(
-      "https://gist.githubusercontent.com/narasimhareddyprostack/7e344f346f47bc53a889d78b5258d0c9/raw/56d531cb936d9c79e2417e5d0e5d8c9c876800f2/contactlist"
-    )
-
+    axios.get("https://gist.githubusercontent.com/narasimhareddyprostack/7e344f346f47bc53a889d78b5258d0c9/raw/56d531cb936d9c79e2417e5d0e5d8c9c876800f2/contactlist")
       .then((res) => {
         setData(res.data)
       })
-
       .catch((err) => {
         console.log(err)
       })
-
   }, [])
 
-
-  let contactinfo = (contact) => {
-
-    alert(`
-      Name : ${contact.name.first}
-      Email : ${contact.email}
-      Phone : ${contact.cell}
-    `)
-
-  }
-
   return (
-
     <div className='container mt-5'>
-
-      <h1>Contact Page</h1>
-
-      {
-        data.length > 0 ?
-
-          <table className='table table-bordered'>
-
-            <thead>
-
-              <tr>
-
-                <th>S.No</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-
-              </tr>
-
-            </thead>
-
-            <tbody>
-
-              {
-                data.map((contact, index) => (
-
-                  <tr
-                    key={index}
-                    onClick={() => contactinfo(contact)}
-                  >
-
-                    <td>{index + 1}</td>
-
-                    <td>{contact.name.first}</td>
-
-                    <td>{contact.email}</td>
-
-                    <td>{contact.cell}</td>
-
+      <h1 className='mb-4'>Contact Page</h1>
+      
+      <div className='row'>
+        <div className='col-lg-7 col-md-6'>
+          {
+            data.length > 0 ?
+              <table className='table table-striped table-hover'>
+                <thead className='table-dark'>
+                  <tr>
+                    <th>S.No</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
                   </tr>
-
-                ))
-              }
-
-            </tbody>
-
-          </table>
-
-          :
-
-          <h2>No Data Available</h2>
-      }
-
+                </thead>
+                <tbody>
+                  {
+                    data.map((contact, index) => (
+                      <tr key={index} onClick={() => setSelectedContact(contact)} style={{ cursor: 'pointer' }}>
+                        <td>{index + 1}</td>
+                        <td>{contact.name.first} {contact.name.last}</td>
+                        <td>{contact.email}</td>
+                        <td>{contact.cell}</td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </table>
+              :
+              <div className='alert alert-info'>
+                <h2>No Data Available</h2>
+              </div>
+          }
+        </div>
+        
+        <div className='col-lg-5 col-md-6'>
+          {
+            selectedContact ? (
+              <div className='card'>
+                <div className='card-header'>
+                  <h2>Contact Information</h2>
+                </div>
+                <div className='card-body'>
+                  <img src={selectedContact.picture.large} alt='contact' className='img-fluid rounded-circle mb-3' />
+                  <p><strong>Name:</strong> {selectedContact.name.first} {selectedContact.name.last}</p>
+                  <p><strong>Email:</strong> {selectedContact.email}</p>
+                  <p><strong>Phone:</strong> {selectedContact.cell}</p>
+                  <p><strong>Location:</strong> {selectedContact.location.city}, {selectedContact.location.state}</p>
+                </div>
+              </div>
+            ) : (
+              <div className='alert alert-secondary'>
+                <h2>Please click on a contact to see details</h2>
+              </div>
+            )
+          }
+        </div>
+      </div>
     </div>
   )
 }
